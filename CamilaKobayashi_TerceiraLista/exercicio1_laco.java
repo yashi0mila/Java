5b5b523114aed83bbd8dfbda99a67b7e6073356b
yashi0mila <133701324+yashi0mila@users.noreply.github.com>
2023-05-16 18:41:05 -0300

    Add files via upload

---
+++ CamilaKobayashi_TerceiraLista/exercicio1_laco.java
@@ -0,0 +1,21 @@
+/*Descrição: Mostra todos os números de 0 a 1.000
+Autora: Camila Gonçalves Kobayashi
+Data: 15.05.2023*/
+
+public class exercicio1_laco{
+
+    public static void main(String[] args){
+
+        //Criação de variáveis
+        int numeros;
+        
+        //Inicialização das variáveis
+        numeros=0;
+
+        while(numeros<=1000){
+            System.out.println("Número: "+ numeros);
+            numeros++;
+        }
+    }
+}
+
