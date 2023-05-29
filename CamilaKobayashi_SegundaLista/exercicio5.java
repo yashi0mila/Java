fa2568d41f890de669cdaa4819bbc94aa78245b2
yashi0mila <133701324+yashi0mila@users.noreply.github.com>
2023-05-29 17:06:08 -0300

    Add files via upload

---
+++ CamilaKobayashi_SegundaLista/exercicio5.java
@@ -0,0 +1,29 @@
+/*Descrição: Descobre o antecessor e o sucessor
+Autora: Camila Gonçalves Kobayashi
+Data: 29.05.2023*/
+
+import java.util.Scanner;
+
+public class exercicio5{
+
+    public static void main(String[] args){
+        
+        Scanner leitor= new Scanner(System.in);
+
+        //Variáveis
+        
+        int n1, res, res2;
+
+        //inicialização
+
+        n1=res=res2=0;
+
+        System.out.println("Digite um número: ");
+        n1=leitor.nextInt();
+
+        res = n1-1;
+        res2 = n1+1;
+
+        System.out.println("A ordem desses números será: " +res+", " +n1+ " e " +res2);
+    }
+}
