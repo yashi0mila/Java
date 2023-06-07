b8a7115b4a25346714a55e36f863d7b25970f355
yashi0mila <133701324+yashi0mila@users.noreply.github.com>
2023-06-07 17:42:35 -0300

    Add files via upload

---
+++ CamilaKobayashi_QuintaLista/exercicio3_casa.java
@@ -0,0 +1,22 @@
+/*Descrição: 10 atributos e 6 métodos para a classe casa
+Autora: Camila Gonçalves Kobayashi
+Data:07.06.2023 */
+
+class casa{
+
+String CorDaCasaDentro, CorDaCasaFora, DataConstrucao, nome, formato;
+float altura, largura, area, idade;
+char tamanho;
+
+void Abrigar()
+
+void Proteger()
+
+void Incendiar()
+
+void Guardar()
+
+void Esconder()
+
+void Explodir()
+}
