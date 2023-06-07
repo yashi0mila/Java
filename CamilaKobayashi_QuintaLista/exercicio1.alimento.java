87b43192bbf6a14a2d85dfe27b98bf69dfa7fc1d
yashi0mila <133701324+yashi0mila@users.noreply.github.com>
2023-06-07 17:25:09 -0300

    Add files via upload

---
+++ CamilaKobayashi_QuintaLista/exercicio1.alimento.java
@@ -0,0 +1,12 @@
+/*Descrição: 10 atributos para a classe alimento
+Autora: Camila Gonçalves Kobayashi
+Data:07.06.2023 */
+
+class Alimento{
+
+String CorDoAlimento, Nome, validade, formato, sabor, aroma;
+float  qntdproteina, qntdcarboidrato, valorcalorico;
+char tamanho;
+
+}
+
