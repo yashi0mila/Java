8e49e6f84671165d5f0691d5f8ff6467a78144dd
yashi0mila <133701324+yashi0mila@users.noreply.github.com>
2023-06-07 17:55:35 -0300

    Add files via upload

---
+++ CamilaKobayashi_QuintaLista/exercicio4_vovoh.java
@@ -0,0 +1,13 @@
+/*Descrição: método construtor para a classe vovoh
+Autora: Camila Gonçalves Kobayashi
+Data:07.06.2023 */
+
+class exercicio4_vovoh{
+    String nome;
+    int idade;
+
+    public vovoh(String nomevovoh, int idadevovoh){
+       nome = nomevovoh;
+       idade = idadevovoh;
+    }
+}
