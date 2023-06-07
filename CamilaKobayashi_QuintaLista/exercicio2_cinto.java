f9127629418507fa26aa268ef0a5b188a87f7393
yashi0mila <133701324+yashi0mila@users.noreply.github.com>
2023-06-07 17:34:11 -0300

    Add files via upload

---
+++ CamilaKobayashi_QuintaLista/exercicio2_cinto.java
@@ -0,0 +1,17 @@
+/*Descrição: 5 métodos para a clase cinto
+Autora: Camila Gonçalves Kobayashi
+Data:07.06.2023 */
+
+class exercicio2_cinto{
+    void Apertar()
+
+    void Segurar()
+
+    void Afivelar()
+    
+    void Existir()
+
+    void Prender()
+    
+}
+
