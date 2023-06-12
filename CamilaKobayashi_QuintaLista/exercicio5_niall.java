ee69dc3a887e77d1c90586684c88d92d2339e93f
yashi0mila <133701324+yashi0mila@users.noreply.github.com>
2023-06-12 20:28:37 -0300

    Add files via upload

---
+++ CamilaKobayashi_QuintaLista/exercicio5_niall.java
@@ -0,0 +1,46 @@
+/*Descrição: classe com 10 atributos, 10 métodos e um método construtor 
+Autora: Camila Gonçalves Kobayashi
+Data:07.06.2023 */
+
+class exercicio5_niall{
+    String nome,CorDosOlhos,CorCabelo,dataNascimento,aroma,cordepele,formatodoolho;
+    char tamanhomao;
+    int idade;
+    double altura,peso;
+
+    public niall(String nomeniall, String corOlho, String corcabelo, String nascimento,String aromaNiall,String peleNiall,String olhoNiall, int idadenialler, char maoniall, double alturaNiall, double pesoNiall){
+       this.nome = nomeniall;
+       this.CorDosOlhos = corOlho;
+       this.CorCabelo = corcabelo;
+       this.dataNascimento = nascimento;
+       this.aroma = aromaNiall;
+       this.cordepele = peleNiall;
+       this.formatodoolho = olhoNiall;
+       this.idade = idadenialler;
+       this.tamanhomao = maoniall;
+       this.altura = alturaNiall;
+       this.peso = pesoNiall;
+
+       class exercicio5_niall{
+        void andar()
+
+        void correr()
+
+        void cantar()
+
+        void dancar()
+
+        void sevestir()
+
+        void nadar()
+
+        void escrever()
+
+        void ler()
+        
+        void desenhar()
+
+        void pintar()
+       }
+    }
+}
